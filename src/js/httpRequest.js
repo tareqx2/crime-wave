1023b0c287a8b53a0d58a7577d95061d831be5b7
Tareq Perez <tareqx3@gmail.com>
2014-12-17 00:37:49 -0600

    address searching

--- src/js/httpRequest.js
+++ src/js/httpRequest.js
@@ -1,18 +1,23 @@
 $( document ).ready(function() {
-	$('button').on("click",function(event){
-		$.get(
-		    "/getCrimes",
-		    {
-		    	lat : map.center.k,
-		    	lon: map.center.D,
-		    	radius: 100
-		    },
-		    function(data) {
-				heatMapper(data);	    	
-		    });
+	$('input').on("keydown",function(event){
+		if(event.keyCode == 13){
+			getCrimes();
+		}
 	});
 });
 
 
-
+function getCrimes(){
+	$.get(
+	    "/getCrimes",
+	    {
+	    	lat : map.center.k,
+	    	lon: map.center.D,
+	    	radius: 100
+	    },
+	    function(data) {
+			heatMapper(data);	    	
+	    }
+	);
+}
 
--- src/js/mapInit.js
+++ src/js/mapInit.js
@@ -14,6 +14,7 @@ function initialize() {
                                        position.coords.longitude);
 
       		map.setCenter(pos);
+          getCrimes();
     	}, function() {
       handleNoGeolocation(map);
     });
